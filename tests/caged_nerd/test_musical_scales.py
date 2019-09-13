b451ccb9f63aaa6b5d2377733a71d34b3bf6575d
LachlanMcLachlan <lachlan.mclachlan@outlook.com>
2019-09-13 20:00:39 +0100

    add tests for chords

--- tests/caged_nerd/test_musical_scales.py
+++ tests/caged_nerd/test_musical_scales.py
@@ -13,6 +13,24 @@ from caged_nerd import musical_scales as mus
         ('a FLAT', 'mixolydian', ['A flat', 'B flat', 'C', 'D flat', 'E flat', 'F', 'G flat', 'A flat'])
     ]
 )
-def test_musical_scales_return_scale(starting_note, mode, expected_scale):
+def test_musical_scales_returns_scale(starting_note, mode, expected_scale):
     scale_object = mus.ScaleCreator(starting_note=starting_note, mode=mode)
     assert scale_object.scale == expected_scale
+
+@pytest.mark.parametrize(
+    argnames='starting_note, mode, expected_chords',
+    argvalues=[
+        ('C', 'major', ['C Major', 'D Minor', 'E Minor', 'F Major', 'G Major',
+                        'A Minor', 'B Dim']),
+        ('f', 'major', ['F Major', 'G Minor', 'A Minor', 'B flat Major',
+                        'C Major', 'D Minor', 'E Dim']),
+        ('f', 'minor', ['F Minor', 'G Dim', 'A flat Major', 'B flat Minor',
+                        'C Minor', 'D flat Major', 'E flat Major']),
+        ('a FLAT', 'mixolydian', ['A flat Major', 'B flat Minor', 'C Dim',
+                                  'D flat Major', 'E flat Minor', 'F Minor',
+                                  'G flat Major'])
+    ]
+)
+def test_musical_scales_returns_chords(starting_note, mode, expected_chords):
+    scale_object = mus.ScaleCreator(starting_note=starting_note, mode=mode)
+    assert scale_object.chords == expected_chords
