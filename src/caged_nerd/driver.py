29c3e72820c547cd248f427d1c916a8c4bbdec69
LachlanMcLachlan <lachlan.mclachlan@outlook.com>
2019-09-21 20:21:39 +0100

    append results to list

--- src/caged_nerd/driver.py
+++ src/caged_nerd/driver.py
@@ -1,23 +1,31 @@
-from caged_nerd.musical_scales import ScaleCreator, CagedMapping
 import random
 import time
+from collections import namedtuple
+
+from caged_nerd.musical_scales import ScaleCreator, CagedMapping
+from caged_nerd.utils import get_execution_time
 
 class ChordsDriver():
     def __init__(self, starting_note, mode, rounds):
         self.starting_note = starting_note
         self.mode = mode
         self.rounds = rounds
+        self._message = 'Play {} in the form {}'
 
     def main(self):
         chords = ScaleCreator(self.starting_note, self.mode).chords
+        ChordFormTime = namedtuple('ChordFormTime', 'chord, form, time')
+        user_results = []
         for i in range(0, self.rounds):
             # make this a random choice with weights
             chord = random.choice(chords)
             form = self._choose_form(chord)
             # user input goes here
-            print(f'Play {chord} in the {form} form')
-            input('Press enter once you\'ve found the chord...)
+            time_to_find_chord = time_user_execution(self._message, chord, form)
+            user_results.append(ChordFormTime(chord, form, time_to_find_chord))
             time.sleep(2)
+
+        print(user_results)
 
 
     def _choose_form(self, chord):
@@ -27,3 +35,10 @@ class ChordsDriver():
             return random.choice(CagedMapping.major)
         else:
             return 'x'
+
+@get_execution_time
+def time_user_execution(message, *args):
+    print(message.format(*args))
+    timed_input = input('Then press enter...')
+
+
