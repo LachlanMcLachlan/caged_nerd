4e5f2925fbd4eacc1d8ff8a6418f8f72727c42a9
LachlanMcLachlan <lachlan.mclachlan@outlook.com>
2019-09-21 20:45:39 +0100

    add black

--- src/caged_nerd/driver.py
+++ src/caged_nerd/driver.py
@@ -1,5 +1,6 @@
 import random
 import time
+import pandas as pd
 from collections import namedtuple
 
 from caged_nerd.musical_scales import ScaleCreator, CagedMapping
@@ -10,11 +11,12 @@ class ChordsDriver():
         self.starting_note = starting_note
         self.mode = mode
         self.rounds = rounds
-        self._message = 'Play {} in the form {}'
+        self._message = 'Play {} in the {} form'
+
+    ChordFormTime = namedtuple('ChordFormTime', 'chord, form, time')
 
     def main(self):
         chords = ScaleCreator(self.starting_note, self.mode).chords
-        ChordFormTime = namedtuple('ChordFormTime', 'chord, form, time')
         user_results = []
         for i in range(0, self.rounds):
             # make this a random choice with weights
@@ -22,10 +24,12 @@ class ChordsDriver():
             form = self._choose_form(chord)
             # user input goes here
             time_to_find_chord = time_user_execution(self._message, chord, form)
-            user_results.append(ChordFormTime(chord, form, time_to_find_chord))
-            time.sleep(2)
+            user_results.append(self.ChordFormTime(chord, form, time_to_find_chord))
+            time.sleep(0)
 
-        print(user_results)
+        df_results = pd.DataFrame.from_records(user_results,
+                                               columns=self.ChordFormTime._fields)
+        print(df_results)
 
 
     def _choose_form(self, chord):
