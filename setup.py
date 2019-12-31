6aff7cccbb8e0d2b3e023539f1993c4dd39ddf46
LachlanMcLachlan <lachlan.mclachlan@outlook.com>
2019-12-31 10:46:30 +0000

    some stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-    Setup file for paint.
+    Setup file for caged nerd.
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 3.2.1.
